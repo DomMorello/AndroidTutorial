0d816e4cd3a994f17292fd3acd53171bb5967d17
Dom_Morello <zuzudnf@gmail.com>
2019-10-16 23:24:58 +0900

    update

---
+++ MyFragment/app/src/main/java/com/example/myfragment/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LinearLayout button1 = (LinearLayout)findViewById(R.id.button1);
+        LinearLayout button2 = (LinearLayout)findViewById(R.id.button2);
+        LinearLayout button3 = (LinearLayout)findViewById(R.id.button3);
+        LinearLayout button4 = (LinearLayout)findViewById(R.id.button4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,new MyFragment1()).commit();
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,new MyFragment2()).commit();
+
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,new MyFragment3()).commit();
+
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame,new MyFragment4()).commit();
+
+            }
+        });
+
+
+    }
+}
