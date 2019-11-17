40f0453fad98219a4e6561178c45e1022e201dbb
Dom_Morello <zuzudnf@gmail.com>
2019-11-17 18:24:35 +0900

    event 공부하기 프로젝트

---
+++ MyEvent/app/src/main/java/com/example/myevent/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.myevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    GestureDetector detector;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+
+        View view = findViewById(R.id.view);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                int action = motionEvent.getAction();
+
+                float curX = motionEvent.getX();
+                float curY = motionEvent.getY();
+                if(action == MotionEvent.ACTION_DOWN){
+                    println("손가락 눌렸음 : " + curX + "," + curY);
+                }else if(action == MotionEvent.ACTION_MOVE){
+                    println("손라락 움직임:" + curX + "," + curY);
+                }else if(action == MotionEvent.ACTION_UP){
+                    println("손가락 떼졌음:" + curX + "," + curY);
+                }
+
+                return true;
+            }
+        });
+
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                println("onDown() 호출됨.");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+                println("onShowPress() 호출됨.");
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                println("onSingleTapUp() 호출됨.");
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onScroll() 호출됨: "+v+","+v1);
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                println("onLongPress() 호출됨.");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onFling() 호출됨: "+v+","+v1);
+                return true;
+            }
+        });
+
+        View view2 = findViewById(R.id.view2);
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                detector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            Toast.makeText(this,"시스템 BACK 버튼 눌림", Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+
+    public void println(String data){
+        textView.append(data + "\n");
+    }
+}
