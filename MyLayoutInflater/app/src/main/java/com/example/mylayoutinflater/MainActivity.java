07aa8074afd2ab58392c9122b963272f53d94efb
Dom_Morello <zuzudnf@gmail.com>
2019-11-23 17:01:12 +0900

    Inflation 연습하기

---
+++ MyLayoutInflater/app/src/main/java/com/example/mylayoutinflater/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mylayoutinflater;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    FrameLayout container;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        container = (FrameLayout)findViewById(R.id.container);
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                inflater.inflate(R.layout.sub1, container, true);
+
+            }
+        });
+    }
+}
