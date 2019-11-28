be6e08bc155acd47a775eeb3710d9603fe364316
Dom_Morello <zuzudnf@gmail.com>
2019-11-28 11:30:33 +0900

    액티비티 라이프사이클

--- MyLifeCycle/app/src/main/java/com/example/mylifecycle/MainActivity.java
+++ MyLifeCycle/app/src/main/java/com/example/mylifecycle/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.mylifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,14 +45,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    //일반적으로 onPause상태에서 필요한 데이터를 저장함.
     protected void onPause() {
         super.onPause();
         Toast.makeText(this,"onPause()호출됨",Toast.LENGTH_SHORT).show();
+
+        //sharedpreferences
+        SharedPreferences pref = getSharedPreferences("pref", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("name","소녀시대");
+        editor.commit();
     }
 
     @Override
+    //앱을 다시 실행시켰을 때 onResume이 호출되면서 아래 SharedPrefernces 에 저장된 값이 복구된다.
     protected void onResume() {
         super.onResume();
         Toast.makeText(this,"onResume()호출됨",Toast.LENGTH_SHORT).show();
+
+        SharedPreferences pref = getSharedPreferences("pref",Activity.MODE_PRIVATE);
+        if(pref != null){
+            String name = pref.getString("name","");
+            Toast.makeText(this,"복구된 이름: "+name, Toast.LENGTH_SHORT).show();
+        }
     }
 }
