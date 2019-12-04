c0afdbe6ad8de78ff29cf74ea2f9b3969c0dccbe
Domas Lee Morello <zuzudnf@gmail.com>
2019-12-04 16:30:56 +0900

    viewPager,animation 연습

--- MyPager/app/src/main/java/com/example/mypager/MainActivity.java
+++ MyPager/app/src/main/java/com/example/mypager/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.mypager;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -8,17 +9,21 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager pager = findViewById(R.id.pager);
+        pager = findViewById(R.id.pager);
         pager.setPageTransformer(true, new ZoomOutPageTransformer());
         pager.setOffscreenPageLimit(3);
 
@@ -33,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
         adapter.addItem(fragment3);
 
         pager.setAdapter(adapter);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pager.setCurrentItem(1);
+            }
+        });
     }
 
     class MoviePagerAdapter extends FragmentStatePagerAdapter{
@@ -56,6 +69,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return items.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "페이지 "+position;
         }
     }
 }
