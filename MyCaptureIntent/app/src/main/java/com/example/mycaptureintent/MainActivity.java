f337494b06588ab2e69c13e8ff9ed2d1c5ff08f1
Domas Lee Morello <zuzudnf@gmail.com>
2019-12-25 20:39:42 +0900

    intent를 이용한 사진촬영 및 저장, 오류 발생

---
+++ MyCaptureIntent/app/src/main/java/com/example/mycaptureintent/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.mycaptureintent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    File file;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        File sdcard = Environment.getExternalStorageDirectory();
+        file = new File("capture.jpg");
+
+        imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capture();
+            }
+        });
+    }
+
+    public void capture(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        startActivityForResult(intent, 101);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == 101 && resultCode == Activity.RESULT_OK){
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;   //8분의1로 크기가 줄어듦.
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(),options);   //파일을 비트맵파일로 디코딩 해줌.
+            imageView.setImageBitmap(bitmap);
+        }
+
+    }
+}
