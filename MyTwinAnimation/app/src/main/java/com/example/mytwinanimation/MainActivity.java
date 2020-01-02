286fb663e32e71060f7a8d6a2a08c43816507994
Domas Lee Morello <zuzudnf@gmail.com>
2020-01-02 12:56:35 +0900

    step8 트윈 에니메이션

---
+++ MyTwinAnimation/app/src/main/java/com/example/mytwinanimation/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mytwinanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation scale = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+                v.startAnimation(scale);
+            }
+        });
+    }
+}
